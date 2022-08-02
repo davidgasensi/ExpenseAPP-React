8dcefb61320dca364b7f407287d82a80d8d85133
David Asensi <b4nedhl@gmail.com>
2022-08-02 15:27:56 +0200

    c

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,7 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 
-const Chart = (props) => {
+const Chart = (propcs) => {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     const totalMaximum = Math.max(...dataPointValues);
 
